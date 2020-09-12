cecbedb6948f490d9377bbc22a8d7ff105b7b54e
Mad Hallé <mad.halle@icloud.com>
2020-09-12 17:12:46 -0600

    db init app added and create method generated

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
 import os
 #create app method
-app = Flask(__name__)
-cors = CORS(app, support_credentials=True, resources={
-    r"/*": {
-        "origins": "*"
-    }
-})
+def create_app():
+    app = Flask(__name__)
+    db = SQLAlchemy()
+    db.init_app(app)
+    cors = CORS(app, support_credentials=True, resources={
+        r"/*": {
+            "origins": "*"
+        }
+    })
 
 # app.config['CORS_HEADERS'] = 'Content-Type'
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 # app.config.from_object(config['FLASK_ENV'])
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy()
 
 class Development(object):
     DEBUG = True
